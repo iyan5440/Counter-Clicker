a7181ac8b09fa3650a754bc6215adb800b07569a
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2022-11-15 00:00:28 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloWorld from './Components/HelloWorld';
+
+function App() {
+  return (
+    <div>
+      <HelloWorld name="test"/>
+    </div>
+  );
+}
+
+export default App;
