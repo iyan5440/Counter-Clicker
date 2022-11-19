3f3fa5707fc48255111f6efc1f99041788c98644
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2022-11-18 21:20:05 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import HelloWorld from './Components/HelloWorld';
+import CounterExample from './Components/CounterExample';
 
 function App() {
   return (
     <div>
-      <HelloWorld name="test"/>
+      {/*<HelloWorld name="tests"/>*/}
+      <CounterExample/>
     </div>
   );
 }
